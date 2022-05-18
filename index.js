4e593926299e520c886dbba0517fb7d31c02a75c
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-19 01:55:14 +0600

    Todo data stored in db

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Using middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3tai1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const todoCollection = client.db('Simple-ToDo').collection('todo-List');
+
+        app.post('/addTodo', async (req, res) => {
+            const todo = req.body;
+            console.log(todo)
+            const result = await todoCollection.insertOne(todo);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
     res.send('Simple todo is up and running')
